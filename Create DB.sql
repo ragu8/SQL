692b04632042d555dd84f485995c8267e03d5ecd
RAGUPATHI-M <ragumdr812@gmail.com>
2022-01-27 19:40:35 +0530

    Create DB

---
+++ Create DB.sql
@@ -0,0 +1,22 @@
+-- Create DB 
+-- The CREATE DATABASE statement is used to create a new SQL database.
+-- Syntax
+
+-- create database DATABASE_NAME;
+
+CREATE DATABASE ZEP;
+
+-- SELECT THE WORKING DB
+
+USE ZEP
+
+-- VIEWING THE DATABASE
+
+SHOW DATABASES;
+
+
+-- The DROP DATABASE statement is used to drop an existing SQL database.
+-- Syntax
+-- DROP DATABASE DATABASE_NAME;
+
+DROP  DATABASE ZEP;
