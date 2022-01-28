acf1af8123c8c71e70fe6cb1a3df8c275537b520
RAGUPATHI-M <ragumdr812@gmail.com>
2022-01-28 07:33:08 +0530

    Basics

---
+++ 03_Alter table.sql
@@ -0,0 +1,34 @@
+-- The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
+-- The ALTER TABLE statement is also used to add and drop various constraints on an existing table.
+
+-- syntax:
+
+/*ALTER TABLE table_name
+ADD column_name datatype; */
+
+ALTER TABLE STD
+ADD INSTA_ID varchar(255);
+
+select * FROM STD;
+
+
+-- DROP COLUMN
+-- SYNTAX
+
+/*ALTER TABLE table_name
+DROP COLUMN column_name;*/
+
+ALTER TABLE STD
+DROP COLUMN INSTA_ID; 
+
+select * FROM STD;
+
+
+-- MODIFY COLUMN
+-- SYNTAX
+
+/* ALTER TABLE table_name
+ALTER COLUMN column_name datatype; */
+
+ALTER TABLE STD
+MODIFY COLUMN E_MAIL VARCHAR(20);
